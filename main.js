3504046d231aa404b6942070877bcea2f176f507
taufikraden29 <taufikraden29@gmail.com>
2024-10-16 14:01:18 +0700

    tugas dom

--- main.js
+++ main.js
@@ -3,8 +3,6 @@
 // document.title = "Belajar Javascript DOM with ME";
 // console.log(document.title);
 
-// const body = document.body;
-
 // body.append("Hello this is Raden Cuy!");
 
 // const h1 = document.createElement("h1");
@@ -23,9 +21,10 @@
 // console.log(document.body);
 
 document.title = "Javascript DOM";
+const body = document.body;
 
 const satu = document.getElementById("satu");
-const dua = document.querySelector(".dua");
+const dua = document.getElementById("dua");
 
 console.log(satu);
 // console.log(dua);
@@ -34,3 +33,30 @@ satu.style.padding = "10px";
 satu.style.fontSize = "20px";
 satu.style.borderRadius = "20px";
 satu.style.backgroundColor = "tomato";
+
+function addText() {
+  const h1 = document.createElement("h1");
+  h1.innerHTML = "Raden Muhamad Irsyad Taufik";
+  body.append(h1);
+}
+
+function textColor() {
+  const h1 = document.createElement("h1");
+  h1.style.color = "red";
+  h1.innerHTML = "Raden Muhamad Irsyad Taufik";
+  body.append(h1);
+}
+
+function gantiWarna() {
+  //   console.log("Aman Bos!");
+  satu.style.backgroundColor = "aqua";
+}
+
+function ubahText() {
+  //   console.log("Ubah text");
+  satu.textContent = "Hihihii";
+}
+
+function oriText() {
+  satu.textContent = "KLIK SAYA 1";
+}
