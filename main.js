00200724830315e07caf27727a640f42951d8511
taufikraden29 <taufikraden29@gmail.com>
2024-10-16 13:17:09 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+// document.title;
+
+// document.title = "Belajar Javascript DOM with ME";
+// console.log(document.title);
+
+// const body = document.body;
+
+// body.append("Hello this is Raden Cuy!");
+
+// const h1 = document.createElement("h1");
+// h1.textContent = "AKU INI H1 LOH BANG!";
+
+// const namaSaya = document.createElement("p");
+// namaSaya.innerHTML = "Raden Muhamad Irsyad Taufik";
+
+// const namaBold = document.createElement("b");
+// namaBold.innerText = "b nih boss, senggol dong!";
+
+// body.append(h1);
+// body.append(namaSaya);
+// body.append(namaBold);
+
+// console.log(document.body);
+
+document.title = "Javascript DOM";
+
+const satu = document.getElementById("satu");
+const dua = document.querySelector(".dua");
+
+console.log(satu);
+// console.log(dua);
+satu.style.border = "none";
+satu.style.padding = "10px";
+satu.style.fontSize = "20px";
+satu.style.borderRadius = "20px";
+satu.style.backgroundColor = "tomato";
